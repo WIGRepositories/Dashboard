b7a533b2b6f730ac0f490bbc70eaeea6ed83242a
spandana.gathram <gathramspandana93@gmail.com>
2016-03-16 19:10:09 +0530

    index changes

--- BTPOSDashboard/UI/js/source.js
+++ BTPOSDashboard/UI/js/source.js
@@ -1,13 +1,28 @@
 
 
 var app = angular.module('myApp', []);
-var ctrl = app.controller('personCtrl', function ($scope, $http) {
-    $http.get('http://localhost:1476/api/dashboard/getdashboard').then(function (response, req) {
-        $scope.Group = response.data;
+var ctrl = app.controller('myCtrl', function ($scope, $http) {
+    $http.get('http://localhost:1476/api/Dashboard/getdashboard').then(function (response, data) {
+       
+        $scope.BTPOS = response.data.Table;
+        $scope.Group = response.data.Table1;
+        $scope.User = response.data.Table2;
+       // $scope.Group = response.data[0];
+      //  $scope.Group = response.data[0];
+       
   
-        $scope.myVar = false;
-        $scope.toggle = function() {
-            $scope.myVar = !$scope.myVar;
-        };
     });
-})
+
+    $scope.myVar = false;
+    $scope.toggle = function () {
+        $scope.myVar = !$scope.myVar;
+    }
+
+});
+
+
+
+
+
+
+
