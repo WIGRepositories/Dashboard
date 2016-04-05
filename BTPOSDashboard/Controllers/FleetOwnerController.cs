1b95a28233f80a18310c6105250303de32113b82
srujansimha <msrujansimha@gmail.com>
2016-04-05 10:49:59 +0530

    fleetowner changes

---
+++ BTPOSDashboard/Controllers/FleetOwnerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BTPOSDashboard.Controllers
+{
+    public class FleetOwnerController : ApiController
+    {
+        [HttpGet]
+        public DataTable GetFleetOwner()//Main Method
+        {
+
+
+            DataTable Tbl = new DataTable();
+            //connect to database
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["btposdb"].ToString();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.StoredProcedure;//Stored Procedure
+            cmd.CommandText = "getFleetOwner";
+            cmd.Connection = conn;
+            DataSet ds = new DataSet();
+            SqlDataAdapter db = new SqlDataAdapter(cmd);
+            db.Fill(ds);
+            Tbl = ds.Tables[0];
+
+            // int found = 0;
+            return Tbl;
+        }
+
+    }
+}
