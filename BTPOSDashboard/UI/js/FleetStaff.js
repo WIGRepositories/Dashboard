dbe95253b92bc1ecf8c1a1f13ab6305447884cd4
srujansimha <msrujansimha@gmail.com>
2016-04-30 09:58:17 +0530

    index

---
+++ BTPOSDashboard/UI/js/FleetStaff.js
@@ -0,0 +1,51 @@
+// JavaScript source code
+// JavaScript source code
+var myapp1 = angular.module('myApp', [])
+var mycrtl1 = myapp1.controller('Mycntrlr', function ($scope, $http) {
+
+
+    $http.get('http://localhost:1476/api/fleetstaff/getfleetstaff').then(function (res, data) {
+        $scope.FleetStaff = res.data;
+
+    });
+
+
+    $scope.save = function (FleetStaff) {
+       
+
+
+        var FleetStaff = {
+
+            Id: FleetStaff.Id,
+            StaffRole: FleetStaff.StaffRole,
+            UserId: FleetStaff.UserId,
+            FromDate: FleetStaff.FromDate,
+            ToDate: FleetStaff.ToDate
+           
+
+
+        };
+
+        var req = {
+            method: 'POST',
+            url: 'http://localhost:1476/api/fleetstaff/savefleetstaff',
+            data: FleetStaff
+        }
+        $http(req).then(function (response) {
+            alert('saved successfully.');
+
+        });
+    //    $scope.currRole = null;
+
+    //};
+
+    //$scope.setCurrRole = function (grp) {
+    //    $scope.currRole = grp;
+    //};
+
+    //$scope.clearCurrRole = function () {
+    //    $scope.currRole = null;
+    };
+
+});
+
