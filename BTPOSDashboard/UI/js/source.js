920a6599a520dd633b2d24a989c4543cf51932b8
srujansimha <msrujansimha@gmail.com>
2016-05-02 22:47:25 +0530

    dashboard changes

--- BTPOSDashboard/UI/js/source.js
+++ BTPOSDashboard/UI/js/source.js
@@ -6,13 +6,13 @@ var ctrl = app.controller('myCtrl', function ($scope, $http, $localStorage) {
 
     //now call GetDashboardDetails and pass userid as parameter
     $http.get('http://localhost:1476/api/dashboard/getdashboard?userid=-1&roleid=-1').then(function (res, data) {
-        $scope.Group = res.data;
+        $scope.dashboardDS = res.data;
     });
 
     $scope.myVar = false;
     $scope.toggle = function () {
         $scope.myVar = !$scope.myVar;
-      
+        document.getElementById('btposstatus').innerHTML = ($scope.myVar) ? "Hide" : "Show";
     };
     $scope.show = function () {
         
