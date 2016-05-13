1f1fe823e827c5defad31009be4b224e57988577
NaveenKumarBeesu <naveenkumarbeesu@gmail.com>
2016-05-13 19:10:56 +0530

    get  and fleetdetails

--- BTPOSDashboard/UI/js/FleetStaff.js
+++ BTPOSDashboard/UI/js/FleetStaff.js
@@ -4,37 +4,38 @@ var myapp1 = angular.module('myApp', [])
 var mycrtl1 = myapp1.controller('Mycntrlr', function ($scope, $http) {
 
 
-    $http.get('http://localhost:1476/api/fleetstaff/getfleetstaff').then(function (res, data) {
+    $http.get('http://localhost:1476/api/FleetStaff/FleetStaff').then(function (res, data) {
         $scope.FleetStaff = res.data;
 
-    });
+    })
+});
 
 
-    $scope.save = function (FleetStaff) {
+    //$scope.save = function (FleetStaff) {
        
 
 
-        var FleetStaff = {
+        //var FleetStaff = {
 
-            Id: FleetStaff.Id,
-            StaffRole: FleetStaff.StaffRole,
-            UserId: FleetStaff.UserId,
-            FromDate: FleetStaff.FromDate,
-            ToDate: FleetStaff.ToDate
+         //   Id: FleetStaff.Id,
+            //StaffRole: FleetStaff.StaffRole,
+            //UserId: FleetStaff.UserId,
+            //FromDate: FleetStaff.FromDate,
+            //ToDate: FleetStaff.ToDate
            
 
 
-        };
+        //};
 
-        var req = {
-            method: 'POST',
-            url: 'http://localhost:1476/api/fleetstaff/savefleetstaff',
-            data: FleetStaff
-        }
-        $http(req).then(function (response) {
-            alert('saved successfully.');
+       // var req = {
+          //  method: 'POST',
+           // url: 'http://localhost:1476/api/fleetstaff/savefleetstaff',
+          //  data: FleetStaff
+        //}
+        //$http(req).then(function (response) {
+           // alert('saved successfully.');
 
-        });
+        //});
     //    $scope.currRole = null;
 
     //};
@@ -45,7 +46,7 @@ var mycrtl1 = myapp1.controller('Mycntrlr', function ($scope, $http) {
 
     //$scope.clearCurrRole = function () {
     //    $scope.currRole = null;
-    };
+    //};
 
-});
+//});
 
