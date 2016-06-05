bbcd63761e5ebd37ad21995da5d37e55b849a0da
NaveenKumarBeesu <naveenkumarbeesu@gmail.com>
2016-06-05 12:18:06 +0530

    ui changes

--- BTPOSDashboard/DBScripts/DBScripts.sql
+++ BTPOSDashboard/DBScripts/DBScripts.sql
@@ -4484,15 +4484,15 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE procedure [dbo].[GetDashboardDetails]
+ALTER procedure [dbo].[GetDashboardDetails]
 (@userid int = -1, @roleid int = -1)
 as
 begin
 --
 --get btpos details
-SELECT b.[Id]
+SELECT  distinct b.[Id]
      -- ,[GroupId]
-    --  ,c.Name as companyname
+      ,c.Name as company
       ,[POSID]
       ,[StatusId]
       ,t.Name as [status]
@@ -4502,9 +4502,9 @@ SELECT b.[Id]
       ,u.FirstName + ' '+ u.LastName as fleetowner
       ,u.Id as fleetownerid
   FROM [POSDashboard].[dbo].[BTPOSDetails] b
-  left outer join Types t on t.Id = statusid
-  left outer join Company c on c.Id = CompanyId
-  left outer join Users u on u.Id = u.companyid
+ left outer join Types t on t.Id = b.StatusId
+ left outer  join Company c on c.Id = b.CompanyId
+  left outer  join Users u on u.CompanyId = c.id
 where (u.id = @userid or @userid = -1)
 
 --get license details
@@ -4536,6 +4536,8 @@ t2.LastName
  inner join Users t2 on t2.Id=t1.UserId
 
 end
+
+
 
 GO
 SET ANSI_NULLS ON
