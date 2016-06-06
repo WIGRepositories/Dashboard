b81e9425e0f6888514d736a4564d8f13b8529c79
srujansimha <msrujansimha@gmail.com>
2016-06-06 08:18:59 +0530

    vehicle layout changes

--- BTPOSDashboard/UI/js/VehicleLayout.js
+++ BTPOSDashboard/UI/js/VehicleLayout.js
@@ -1,13 +1,13 @@
 ﻿// JavaScript source code
 var myapp1 = angular.module('myApp', ['ngStorage'])
-var mycrtl1 = myapp1.controller('Mycntrlr', function ($scope, $http, $localStorage) {
+var mycrtl1 = myapp1.controller('Mycntrlr', function ($scope, $http, $localStorage, $filter) {
     $scope.uname = $localStorage.uname;
 
     $scope.GetVehicleConfig = function () {
 
         var vc = {
             needvehicleType: '1',
-            needvehiclelayout: '1',
+            needvehiclelayout: '1'
         };
 
         var req = {
@@ -35,18 +35,72 @@ var mycrtl1 = myapp1.controller('Mycntrlr', function ($scope, $http, $localStora
     $scope.displayLayout = function () {
         var container = document.getElementById('basic_example');
 
-        var data = function () {
-            return Handsontable.helper.createSpreadsheetData(eval($scope.rows), eval($scope.cols));
-        };
+           // rowCount = typeof rowCount === 'number' ? rowCount : 4;
+        //  colCount = typeof colCount === 'number' ? colCount : 13;
 
-        var hot = new Handsontable(container, {
-            data: data(),
-            height: 396,
-            colHeaders: false,
-            rowHeaders: false,
-            stretchH: 'all',
-            columnSorting: true,
-            contextMenu: true
+        rowCount = document.getElementById('rowSelected').value;
+        colCount = document.getElementById('colSelected').value;
+            var rows = [],
+                i,
+                j;
+            for (i = 0; i < rowCount; i++) {
+                var row = [];
+                for (j = 0; j < colCount; j++) {
+                    row.push({"Id":spreadsheetColumnLabel(j) + (i + 1),"selected":true});
+                }
+                rows.push(row);
+            }
+            
+
+            $scope.datarows = rows;
+
+        //function getData(rowCount, colCount) {
+
+        //    rowCount = typeof rowCount === 'number' ? rowCount : 100;
+        //    colCount = typeof colCount === 'number' ? colCount : 4;
+        //    var rows = [],
+        //        i,
+        //        j;
+        //    for (i = 0; i < rowCount; i++) {
+        //        var row = [];
+        //        for (j = 0; j < colCount; j++) {
+        //            row.push(spreadsheetColumnLabel(j) + (i + 1));
+        //        }
+        //        rows.push(row);
+        //    }
+        //    return rows;
+        //}
+
+        function spreadsheetColumnLabel(index) {
+            var dividend = index + 1;
+            var columnLabel = '';
+            var modulo;
+            while (dividend > 0) {
+                modulo = (dividend - 1) % 26;
+                columnLabel = String.fromCharCode(65 + modulo) + columnLabel;
+                dividend = parseInt((dividend - modulo) / 26, 10);
+            }
+            return columnLabel;
+        }
+
+        //var hot = new Handsontable(container, {
+        //    data: getData(3,8),
+        //    startRows:8,
+        //    height: 396,           
+        //    rowHeaders: true,
+        //    colHeaders: true,
+        //  //  stretchH: 'all',            
+        //    columnSorting: true,
+        //    contextMenu: true           
+        //});
+    }
+
+    $scope.saveVehicleLayout = function () {
+
+        var savedate = $scope.datarows;
+        $scope.checkedArr = $scope.datarows.filter(function (person) {
+
+            return (function (p) { return p.Id == "A1" });
         });
     }
 
