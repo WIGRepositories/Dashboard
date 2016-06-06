0176cba5d9d48e4e251cb60df59f4dd4ae4b0ee5
NaveenKumarBeesu <naveenkumarbeesu@gmail.com>
2016-06-06 15:42:23 +0530

    todays changes by yamini

---
+++ BTPOSDashboard/UI/js/FleetOwnerVehicleLayout.js
@@ -0,0 +1,108 @@
+﻿// JavaScript source code
+var myapp1 = angular.module('myApp', ['ngStorage'])
+var mycrtl1 = myapp1.controller('Mycntrlr', function ($scope, $http, $localStorage, $filter) {
+    $scope.uname = $localStorage.uname;
+
+    $scope.GetVehicleConfig = function () {
+
+        var vc = {
+            needvehicleType: '1',
+            needvehiclelayout: '1'
+        };
+
+        var req = {
+            method: 'POST',
+            url: 'http://localhost:1476/api/VehicleConfig/VConfig',
+            //headers: {
+            //    'Content-Type': undefined
+
+            data: vc
+        }
+        $http(req).then(function (res) {
+            $scope.initdata = res.data;
+        });
+
+    }
+
+
+    $scope.getselectval = function (vlType) {
+        if (vlType == null) {
+            $scope.vlConfig = null;
+            return;
+        }
+    }
+
+    $scope.displayLayout = function () {
+        var container = document.getElementById('basic_example');
+
+        // rowCount = typeof rowCount === 'number' ? rowCount : 4;
+        //  colCount = typeof colCount === 'number' ? colCount : 13;
+
+        rowCount = document.getElementById('rowSelected').value;
+        colCount = document.getElementById('colSelected').value;
+        var rows = [],
+            i,
+            j;
+        for (i = 0; i < rowCount; i++) {
+            var row = [];
+            for (j = 0; j < colCount; j++) {
+                row.push({ "Id": spreadsheetColumnLabel(j) + (i + 1), "selected": true });
+            }
+            rows.push(row);
+        }
+
+
+        $scope.datarows = rows;
+
+        //function getData(rowCount, colCount) {
+
+        //    rowCount = typeof rowCount === 'number' ? rowCount : 100;
+        //    colCount = typeof colCount === 'number' ? colCount : 4;
+        //    var rows = [],
+        //        i,
+        //        j;
+        //    for (i = 0; i < rowCount; i++) {
+        //        var row = [];
+        //        for (j = 0; j < colCount; j++) {
+        //            row.push(spreadsheetColumnLabel(j) + (i + 1));
+        //        }
+        //        rows.push(row);
+        //    }
+        //    return rows;
+        //}
+
+        function spreadsheetColumnLabel(index) {
+            var dividend = index + 1;
+            var columnLabel = '';
+            var modulo;
+            while (dividend > 0) {
+                modulo = (dividend - 1) % 26;
+                columnLabel = String.fromCharCode(65 + modulo) + columnLabel;
+                dividend = parseInt((dividend - modulo) / 26, 10);
+            }
+            return columnLabel;
+        }
+
+        //var hot = new Handsontable(container, {
+        //    data: getData(3,8),
+        //    startRows:8,
+        //    height: 396,           
+        //    rowHeaders: true,
+        //    colHeaders: true,
+        //  //  stretchH: 'all',            
+        //    columnSorting: true,
+        //    contextMenu: true           
+        //});
+    }
+
+    $scope.saveVehicleLayout = function () {
+
+        var savedate = $scope.datarows;
+        $scope.checkedArr = $scope.datarows.filter(function (person) {
+
+            return (function (p) { return p.Id == "A1" });
+        });
+    }
+
+
+});
