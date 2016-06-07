f972a725f265227dc21683a278557769216ab480
NaveenKumarBeesu <naveenkumarbeesu@gmail.com>
2016-06-07 16:45:52 +0530

    fleetowner vehicle schedule changes

---
+++ BTPOSDashboard/Controllers/FleetOwnerVehicleScheduleController.cs
@@ -0,0 +1,57 @@
+﻿using BTPOSDashboardAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data;
+using System.Data.SqlClient;
+namespace BTPOSDashboard.Controllers
+{
+    public class FleetOwnerVehicleScheduleController : ApiController
+    {
+       public DataTable getroutedetails(int sId, int cmpid, int routeid)
+        {
+            DataTable Tbl = new DataTable();
+
+
+            //connect to database
+            SqlConnection conn = new SqlConnection();
+            //connetionString="Data Source=ServerName;Initial Catalog=DatabaseName;User ID=UserName;Password=Password"
+            conn.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["btposdb"].ToString();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "getRouteDetails";
+            cmd.Connection = conn;
+            DataSet ds = new DataSet();
+            SqlDataAdapter db = new SqlDataAdapter(cmd);
+
+            SqlParameter fo = new SqlParameter();
+            fo.ParameterName = "@fleetownerId";
+            fo.SqlDbType = SqlDbType.Int;
+            fo.Value = sId;
+            cmd.Parameters.Add(fo);
+
+            SqlParameter fsc = new SqlParameter();
+            fsc.ParameterName = "@cmpId";
+            fsc.SqlDbType = SqlDbType.Int;
+            fsc.Value = cmpid;
+            cmd.Parameters.Add(fsc);
+
+            SqlParameter cid = new SqlParameter();
+            cid.ParameterName = "@routeid";
+            cid.SqlDbType = SqlDbType.Int;
+            cid.Value = routeid;
+            cmd.Parameters.Add(cid);
+
+            db.Fill(ds);
+           Tbl = ds.Tables[0];
+
+           // int found = 0;
+            return Tbl;
+       }
+           
+    }
+}
---
+++ BTPOSDashboard/UI/js/FleetOwnerVehicleSchedule.js
@@ -0,0 +1,78 @@
+﻿var myapp1 = angular.module('myApp', [])
+
+var mycrtl1 = myapp1.controller('Mycntrlr', function ($scope, $http) {
+
+    $scope.GetCompanies = function () {
+
+        var vc = {
+            needCompanyName: '1'
+        };
+
+        var req = {
+            method: 'POST',
+            url: 'http://localhost:1476/api/VehicleConfig/VConfig',
+            //headers: {
+            //    'Content-Type': undefined
+            data: vc
+        }
+        $http(req).then(function (res) {
+            $scope.initdata = res.data;
+        });
+
+    }
+
+    $scope.GetFleetOwners = function () {
+        if ($scope.cmp == null) {
+            $scope.FleetOwners = null;
+            return;
+        }
+        var vc = {
+            needfleetowners: '1',
+            cmpId: $scope.cmp.Id
+        };
+
+        var req = {
+            method: 'POST',
+            url: 'http://localhost:1476/api/VehicleConfig/VConfig',
+            //headers: {
+            //    'Content-Type': undefined
+
+            data: vc
+
+
+        }
+        $http(req).then(function (res) {
+            $scope.cmpdata = res.data;
+        });
+    }
+    $scope.GetRouteDetails = function () {
+        if ($scope.s == null) {
+            $scope.Routes = null;
+            return;
+        }
+        // $http.get('http://localhost:1476/api/FleetOwnerVehicleSchedule/GetRouteDetails?cmpId=' + $scope.cmp.Id + '&fleetownerId=' + $scope.s.Id).then(function (res, data) {
+        //   $scope.Routes = res.data;
+
+        // });
+
+
+        var vc = {
+            needroutes: '1',
+            sId: $scope.s.Id
+        };
+
+        var req = {
+            method: 'POST',
+            url: 'http://localhost:1476/api/VehicleConfig/VConfig',
+            //headers: {
+            //    'Content-Type': undefined
+
+            data: vc
+
+
+        }
+        $http(req).then(function (res) {
+            $scope.sdata = res.data;
+        });
+    }
+});
