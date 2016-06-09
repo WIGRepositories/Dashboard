f66e2a503545a5424f00f38ef898680406b2840b
NaveenKumarBeesu <naveenkumarbeesu@gmail.com>
2016-06-09 18:19:35 +0530

    fleet changes

--- BTPOSDashboard/UI/js/RouteFare.js
+++ BTPOSDashboard/UI/js/RouteFare.js
@@ -3,21 +3,10 @@ var myapp1 = angular.module('myApp', [])
 
 var mycrtl1 = myapp1.controller('Mycntrlr', function ($scope, $http) {
 
-    $scope.routefares =
-        [
-          {
-              "Rows": "Hyderabad", "Hyderabad": 0, "Vijaywada": 307, "Kurnool": 400, "Karimnagar": 200
-          },
-          {
-              "Rows": "Vijaywada", "Hyderabad": 307, "Vijaywada": 0, "Kurnool": 100, "Karimnagar": 500
-          },
-          {
-              "Rows": "Kurnool", "Hyderabad": 400, "Vijaywada": 100, "Kurnool": 0, "Karimnagar": 300
-          },
-          {
-              "Rows": "Karimnagar","Hyderabad": 200,"Vijaywada": 500,"Kurnool": 300,"Karimnagar": 0
-          }
-        ];
+    $scope.routefares =function () {
+        $http.get('http://localhost:1476/api/RouteFare/getRouteFare?routeId=2&fleetownerId=2').then(function (res, data) {
+            $scope.Stops = res.data;
+        });
 
     $scope.GetStops = function () {
         $http.get('http://localhost:1476/api/Stops/GetStops').then(function (res, data) {
