f7f319bf8547e0b75a78c57b614722aeb935f4d3
NaveenKumarBeesu <naveenkumarbeesu@gmail.com>
2016-06-15 18:33:42 +0530

    license payments changes

---
+++ BTPOSDashboard/UI/js/UserLicense.js
@@ -0,0 +1,25 @@
+﻿var app = angular.module('myApp', ['ngStorage'])
+
+var ctrl = app.controller('Myctrlr', function ($scope, $http, $localStorage) {
+    $scope.uname = $localStorage.uname;
+
+    /* user details functions */
+    $scope.uname = $localStorage.uname;
+    $http.get('http://localhost:1476/api/GetLicensePayments').then(function (response, req) {
+        $scope.Group = response.data;
+
+    });
+
+
+    $scope.gridOptions = {
+        data: 'sampledata',
+        columnDefs: [
+        { field: 'name' },
+        { field: 'gender' },
+        { field: 'company' },
+        { field: 'click', cellTemplate: '<button class="btn primary" ng-click="grid.appScope.sampledetails()">Click Me</button>' }
+        ]
+    };
+
+});
+   
