23674ccad8ac4ec4d6ee188542d18c4bb7810b44
pmunipavani <pmunipavani@gmail.com>
2016-06-22 17:58:29 +0530

    add fleet owner(edit)

--- BTPOSDashboard/UI/js/FleetOwners.js
+++ BTPOSDashboard/UI/js/FleetOwners.js
@@ -9,7 +9,7 @@ var ctrl = app.controller('myCtrl', function ($scope, $http, $localStorage) {
             $scope.FleetOwner = res.data;
         });
     }
-   
+
     $scope.setUsers = function (usr) {
         $scope.User1 = usr;
     };
@@ -18,100 +18,207 @@ var ctrl = app.controller('myCtrl', function ($scope, $http, $localStorage) {
         $scope.User1 = null;
     }
 
+    $scope.setUsers = function (usr) {
+        $scope.User1 = usr;
+    };
 
-    $scope.save = function (Fleet) {
-        if (Fleet == null) {
-            alert('Please enter VehicleRegNo.');
-            return;
-        }
-
-        if (Fleet.Name == null) {
-            alert('Please enter VehicleRegNo.');
-            return;
-        }
-        if (Fleet.group == null || Fleet.VehicleTypeId.group.Id == null) {
-            alert('Please select a type group');
-            return;
-        }
-
-
-
-        var Fleet = {
-            Id: Fleet.Id,
-            Name: Fleet.Name,
-            Company: Fleet.Company,
-            FleetOwnerCode: Fleet.FleetOwnerCode,
-            Active: Fleet.Active,
-           
-
-        };
-
-        var req = {
-            method: 'POST',
-            url: 'http://localhost:1476/api/Fleet/NewFleetDetails',
-            //headers: {
-            //    'Content-Type': undefined
-
-            data: Fleet
-
-
-        }
-        $http(req).then(function (res) { });
-
-
+    $scope.clearUsers = function () {
+        $scope.User1 = null;
     }
 
-    $scope.savenewfleetdetails = function (initdata) {
-        var newVD = initdata.newfleet;
-        /* if (newVD == null) {
-             alert('Please enter VehicleRegNo.');
-             return;
-         }
- 
-         if (newVD.VehicleRegNo == null) {
-             alert('Please enter VehicleRegNo.');
-             return;
-         }
-         if (Fleet.group == null || Fleet.VehicleTypeId.group.Id == null) {
-             alert('Please select a type group');
-             return;
-         }
-         */
+    $scope.saveCmpChanges = function (Fleet, flag) {
 
+        //if (Fleet == null) {
+           // alert('Please enter CompanyName.');
+           // return;
+       // }
+        //if (Fleet.FirstName == null || Group.FirstName == "") {
+          //  alert('Please enter CompanyName.');
+          //  return;
+        //}
 
         var Fleet = {
             Id: Fleet.Id,
-            Name: Fleet.Name,
-            Company: Fleet.Company,
-            FleetOwnerCode: Fleet.FleetOwnerCode,
+            FirstName: Fleet.FirstName,
+            LastName: Fleet.LastName,
+            MiddleName: Fleet.MiddleName,
+            EmployeeNo: Fleet.EmpNo,
+            Email: Fleet.Email,
+            MobileNo: Fleet.MobileNo,
             Active: Fleet.Active,
+            UserName: Fleet.UserName,
+            Password: Fleet.Password,
+            RePassword: Fleet.Password,
+            insupdflag: flag
 
-        };
+
+        }
+
 
         var req = {
             method: 'POST',
-            url: 'http://localhost:1476/api/Fleet/NewFleetDetails',
-            //headers: {
-            //    'Content-Type': undefined
-
+            url: 'http://localhost:1476/api/users/saveusers',
             data: Fleet
         }
+        //        $http(req).then(function (response) {
+
+        //            $scope.showDialog("Saved successfully!");
+
+
+        //        })
+        //    }
+        //}
+        //});
 
         $http(req).then(function (response) {
 
             $scope.showDialog("Saved successfully!");
 
-            $scope.Group = null;
+        })
+     , function (errres) {
+         var errdata = errres.data;
+         var errmssg = "";
+         errmssg = (errdata && errdata.ExceptionMessage) ? errdata.ExceptionMessage : errdata.Message;
+         $scope.showDialog(errmssg);
 
-        }, function (errres) {
-            var errdata = errres.data;
-            var errmssg = "";
-            errmssg = (errdata && errdata.ExceptionMessage) ? errdata.ExceptionMessage : errdata.Message;
-            $scope.showDialog(errmssg);
+
+         $scope.GetFleetOwner();
+         $scope.User1 = null;
+     };
+
+
+        $scope.setCompany = function (grp) {
+            $scope.User1 = grp;
+        };
+
+        //$scope.clearGroup = function () {
+          //  $scope.User1 = null;
+        //};
+
+        $scope.showDialog = function (message) {
+
+            var modalInstance = $uibModal.open({
+                animation: $scope.animationsEnabled,
+                templateUrl: 'myModalContent.html',
+                controller: 'ModalInstanceCtrl',
+                resolve: {
+                    mssg: function () {
+                        return message;
+                    }
+                }
+            });
+        }
+
+
+        app.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, mssg) {
+
+            $scope.mssg = mssg;
+            $scope.ok = function () {
+                $uibModalInstance.close('test');
+            };
+
+            $scope.cancel = function () {
+                $uibModalInstance.dismiss('cancel');
+            };
         });
 
     }
 });
+    
+
+//    $scope.save = function (Fleet) {
+//        if (Fleet == null) {
+//            alert('Please enter VehicleRegNo.');
+//            return;
+//        }
+
+//        if (Fleet.Name == null) {
+//            alert('Please enter VehicleRegNo.');
+//            return;
+//        }
+//        //if (Fleet.group == null || Fleet.VehicleTypeId.group.Id == null) {
+//          //  alert('Please select a type group');
+//            //return;
+//        //}
+
+
+
+//        var Fleet = {
+//            Id: Fleet.Id,
+//            FirstName: Fleet.FirstName,
+//            LastName: Fleet.LastName,
+//            FleetOwnerCode: Fleet.FleetOwnerCode,
+//            Active: Fleet.Active,
+           
+
+//        };
+
+//        var req = {
+//            method: 'POST',
+//            url: 'http://localhost:1476/api/Fleet/NewFleetDetails',
+//            //headers: {
+//            //    'Content-Type': undefined
+
+//            data: Fleet
+
+
+//        }
+//        $http(req).then(function (res) { });
+
+
+//    }
+
+//    $scope.savenewfleetdetails = function (initdata) {
+//        var newVD = initdata.newfleet;
+//        /* if (newVD == null) {
+//             alert('Please enter VehicleRegNo.');
+//             return;
+//         }
+ 
+//         if (newVD.VehicleRegNo == null) {
+//             alert('Please enter VehicleRegNo.');
+//             return;
+//         }
+//         if (Fleet.group == null || Fleet.VehicleTypeId.group.Id == null) {
+//             alert('Please select a type group');
+//             return;
+//         }
+//         */
+
+
+//        var Fleet = {
+//            Id: Fleet.Id,
+//            Name: Fleet.Name,
+//            Company: Fleet.Company,
+//            FleetOwnerCode: Fleet.FleetOwnerCode,
+//            Active: Fleet.Active,
+
+//        };
+
+//        var req = {
+//            method: 'POST',
+//            url: 'http://localhost:1476/api/Fleet/NewFleetDetails',
+//            //headers: {
+//            //    'Content-Type': undefined
+
+//            data: Fleet
+//        }
+
+//        $http(req).then(function (response) {
+
+//            $scope.showDialog("Saved successfully!");
+
+           
+
+//        }, function (errres) {
+//            var errdata = errres.data;
+//            var errmssg = "";
+//            errmssg = (errdata && errdata.ExceptionMessage) ? errdata.ExceptionMessage : errdata.Message;
+//            $scope.showDialog(errmssg);
+//        });
+
+//    }
+//});
 
 
 
