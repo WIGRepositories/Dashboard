bb31dd01b7aa0e8f81beff845f4ef040e3805b97
LOKESH1231 <lokesh.nandikunta@gmail.com>
2016-06-28 11:51:34 +0530

    Data Changes

--- BTPOSDashboard/UI/js/source.js
+++ BTPOSDashboard/UI/js/source.js
@@ -4,7 +4,7 @@ var ctrl = app.controller('myCtrl', function ($scope, $http, $localStorage) {
     //{
     //    window.location.href = "login.html";
     //}
-    //$scope.uname = $localStorage.uname;
+    $scope.uname = $localStorage.uname;
     //if ($localStorage.userdetails && $localStorage.userdetails.length > 0 && $localStorage.userdetails[0])
     //$scope.userid = $localStorage.userdetails[0].userid;
 
@@ -14,6 +14,7 @@ var ctrl = app.controller('myCtrl', function ($scope, $http, $localStorage) {
         $http.get('http://localhost:1476/api/dashboard/getdashboard?userid=-1&roleid=-1').then(function (res, data)
         {
             $scope.dashboardDS = res.data;
+            $localStorage.dashboardDS = res.data;
         });
     }
     $scope.myVar = false;
