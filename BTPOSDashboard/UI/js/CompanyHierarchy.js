b07776a1ef7a903494379a68c58f8ae45ccce3dd
srujansimha <msrujansimha@gmail.com>
2016-07-06 18:45:19 +0530

    added company hierarchy file

---
+++ BTPOSDashboard/UI/js/CompanyHierarchy.js
@@ -0,0 +1,16 @@
+﻿var app = angular.module('myApp', ['ngStorage']);
+
+var ctrl = app.controller('myCtrl', function ($scope, $http, $localStorage) {
+    // $scope.uname = $localStorage.uname;
+    $scope.userdetails = $localStorage.userdetails;
+    $scope.Roleid = $scope.userdetails[0].roleid;
+
+    $scope.dashboardDS = $localStorage.dashboardDS;
+
+    $scope.GetCompanys = function () {
+        $http.get('http://localhost:1476/api/GetCompanyGroups?userid=-1').then(function (response, data) {
+            $scope.Companies = response.data;
+
+        });
+    }
+});
