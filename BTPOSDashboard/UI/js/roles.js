c16a42e27b92070b347816c50e0afc8949efd8bd
LOKESH1231 <lokesh.nandikunta@gmail.com>
2016-07-25 17:44:48 +0530

    ui changes

--- BTPOSDashboard/UI/js/roles.js
+++ BTPOSDashboard/UI/js/roles.js
@@ -152,6 +152,13 @@ var mycrtl1 = myapp1.controller('myCtrl', function ($scope, $http, $localStorage
         });
        
     }
+    
+        //This will hide the DIV by default.
+        $scope.IsHidden = true;
+        $scope.ShowHide = function () {
+            //If DIV is hidden it will be visible and vice versa.
+            $scope.IsHidden = $scope.IsHidden ? false : true;
+        }
 
 
    
