7b7bb45a9f15638835d0dd35eaa9c51c1a71ef21
srujansimha <msrujansimha@gmail.com>
2016-07-29 18:07:05 +0530

    CHANGES

--- BTPOSDashboard/UI/js/FleetDetail.js
+++ BTPOSDashboard/UI/js/FleetDetail.js
@@ -77,6 +77,13 @@ var ctrl = app.controller('myCtrl', function ($scope, $http, $localStorage, $uib
         });
     }
 
+    //This will hide the DIV by default.
+    $scope.IsHidden = true;
+    $scope.ShowHide = function () {
+        //If DIV is hidden it will be visible and vice versa.
+        $scope.IsHidden = $scope.IsHidden ? false : true;
+    }
+
 
     //$scope.GetCompanies = function () {
 
