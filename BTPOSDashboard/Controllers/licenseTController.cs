767aeaffa4f31263a350314e0a52808488b60574
srujansimha <msrujansimha@gmail.com>
2016-08-20 14:01:17 +0530

    changes

--- BTPOSDashboard/Controllers/licenseTController.cs
+++ BTPOSDashboard/Controllers/licenseTController.cs
@@ -42,88 +42,88 @@ namespace BTPOSDashboard.Controllers
             return Tbl;
         }
 
-          [HttpPost]
-        public HttpResponseMessage SaveLicensePricing(LicensePricing b)
-       {
-            SqlConnection conn = new SqlConnection();
-            try
-            {
+       //   [HttpPost]
+       // public HttpResponseMessage SaveLicensePricing(LicensePricing b)
+       //{
+       //     SqlConnection conn = new SqlConnection();
+       //     try
+       //     {
 
-            //connect to database
+       //     //connect to database
             
-            //connetionString="Data Source=ServerName;Initial Catalog=DatabaseName;User ID=UserName;Password=Password"
-            conn.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["btposdb"].ToString();
+       //     //connetionString="Data Source=ServerName;Initial Catalog=DatabaseName;User ID=UserName;Password=Password"
+       //     conn.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["btposdb"].ToString();
 
-            SqlCommand cmd = new SqlCommand();
-            cmd.CommandType = CommandType.StoredProcedure;
-            cmd.CommandText = "InsUpdDelLicensePricing";
-            cmd.Connection = conn;
-            conn.Open();
-            SqlParameter Aid = new SqlParameter();
-            Aid.ParameterName = "@Id";
-            Aid.SqlDbType = SqlDbType.Int;
-            Aid.Value = Convert.ToString(b.Id);
-            cmd.Parameters.Add(Aid);
+       //     SqlCommand cmd = new SqlCommand();
+       //     cmd.CommandType = CommandType.StoredProcedure;
+       //     cmd.CommandText = "InsUpdDelLicensePricing";
+       //     cmd.Connection = conn;
+       //     conn.Open();
+       //     SqlParameter Aid = new SqlParameter();
+       //     Aid.ParameterName = "@Id";
+       //     Aid.SqlDbType = SqlDbType.Int;
+       //     Aid.Value = Convert.ToString(b.Id);
+       //     cmd.Parameters.Add(Aid);
            
-            SqlParameter lid = new SqlParameter();
-            lid.ParameterName = "@LicenseId";
-            lid.SqlDbType = SqlDbType.Int;
-            lid.Value = Convert.ToString(b.LicenseId);
-            cmd.Parameters.Add(lid);
+       //     SqlParameter lid = new SqlParameter();
+       //     lid.ParameterName = "@LicenseId";
+       //     lid.SqlDbType = SqlDbType.Int;
+       //     lid.Value = Convert.ToString(b.LicenseId);
+       //     cmd.Parameters.Add(lid);
 
-            SqlParameter ss = new SqlParameter();
-            ss.ParameterName = "@RenewalFreqTypeId";
-            ss.SqlDbType = SqlDbType.Int;
-            ss.Value = b.RenewalFreqTypeId;
-            cmd.Parameters.Add(ss);
+       //     SqlParameter ss = new SqlParameter();
+       //     ss.ParameterName = "@RenewalFreqTypeId";
+       //     ss.SqlDbType = SqlDbType.Int;
+       //     ss.Value = b.RenewalFreqTypeId;
+       //     cmd.Parameters.Add(ss);
 
-            SqlParameter pid = new SqlParameter();
-            pid.ParameterName = "@RenewalFreq";
-            pid.SqlDbType = SqlDbType.Int;
-            pid.Value = b.RenewalFreq;
-            cmd.Parameters.Add(pid);
+       //     SqlParameter pid = new SqlParameter();
+       //     pid.ParameterName = "@RenewalFreq";
+       //     pid.SqlDbType = SqlDbType.Int;
+       //     pid.Value = b.RenewalFreq;
+       //     cmd.Parameters.Add(pid);
 
-            SqlParameter sid = new SqlParameter();
-            sid.ParameterName = "@UnitPrice";
-            sid.SqlDbType = SqlDbType.Decimal;
-            sid.Value = b.UnitPrice;
-            cmd.Parameters.Add(sid);
+       //     SqlParameter sid = new SqlParameter();
+       //     sid.ParameterName = "@UnitPrice";
+       //     sid.SqlDbType = SqlDbType.Decimal;
+       //     sid.Value = b.UnitPrice;
+       //     cmd.Parameters.Add(sid);
 
-            SqlParameter gid = new SqlParameter();
-            gid.ParameterName = "@todate";
-            gid.SqlDbType = SqlDbType.DateTime;
-            gid.Value = b.todate;
-            cmd.Parameters.Add(gid);
+       //     SqlParameter gid = new SqlParameter();
+       //     gid.ParameterName = "@todate";
+       //     gid.SqlDbType = SqlDbType.DateTime;
+       //     gid.Value = b.todate;
+       //     cmd.Parameters.Add(gid);
 
-            SqlParameter fid = new SqlParameter();
-            fid.ParameterName = "@fromdate";
-            fid.SqlDbType = SqlDbType.DateTime;
-            fid.Value = b.fromdate;
-            cmd.Parameters.Add(fid);
+       //     SqlParameter fid = new SqlParameter();
+       //     fid.ParameterName = "@fromdate";
+       //     fid.SqlDbType = SqlDbType.DateTime;
+       //     fid.Value = b.fromdate;
+       //     cmd.Parameters.Add(fid);
 
 
-            SqlParameter nActive = new SqlParameter("@insupddelflag", SqlDbType.Char);
-            nActive.Value = b.insupddelflag;
-            cmd.Parameters.Add(nActive);
+       //     SqlParameter nActive = new SqlParameter("@insupddelflag", SqlDbType.Char);
+       //     nActive.Value = b.insupddelflag;
+       //     cmd.Parameters.Add(nActive);
 
-            cmd.ExecuteScalar();
-            conn.Close();
+       //     cmd.ExecuteScalar();
+       //     conn.Close();
              
 
-             //int found = 0;
-            return new HttpResponseMessage(HttpStatusCode.OK);
-            }
-            catch (Exception ex)
-            {
-                if (conn != null && conn.State == ConnectionState.Open)
-                {
-                    conn.Close();
-                }
-                string str = ex.Message;
-                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
-            }
-       }
+       //      //int found = 0;
+       //     return new HttpResponseMessage(HttpStatusCode.OK);
+       //     }
+       //     catch (Exception ex)
+       //     {
+       //         if (conn != null && conn.State == ConnectionState.Open)
+       //         {
+       //             conn.Close();
+       //         }
+       //         string str = ex.Message;
+       //         return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+       //     }
+       //}
 
-          public void Options() { }
+       //   public void Options() { }
     }
 }
